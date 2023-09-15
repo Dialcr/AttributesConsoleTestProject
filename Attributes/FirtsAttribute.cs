fc5898bc725c3bacae3e406097ba9d3f807ce462
diego.ac <diego.ac@factory.generalsoftwareinc.com>
2023-09-15 11:56:51 -0400

    first commit in to rider

---
+++ Attributes/FirtsAttribute.cs
@@ -0,0 +1,26 @@
+﻿namespace AttributesConsoleTestProject.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class FirtsAttribute : Attribute
+{
+    private readonly int maxLength;
+
+    public FirtsAttribute(int maxLength)
+    {
+        this.maxLength = maxLength;
+    }
+
+
+    public void OnEntry(string input)
+    {
+        if (input == null)
+        {
+            throw new ArgumentNullException(nameof(input), "El valor no puede ser nulo.");
+        }
+
+        if (input.Length > maxLength)
+        {
+            throw new ArgumentException($"La longitud del valor debe ser menor o igual a {maxLength} caracteres.", nameof(input));
+        }
+    }
+}
---
+++ Methods/Method.cs
@@ -0,0 +1,13 @@
+﻿using AttributesConsoleTestProject.Attributes;
+namespace AttributesConsoleTestProject.Methods;
+
+public static class Method
+{
+    [Firts(5)]
+    public static void WriteWord(string word)
+    {
+        Console.WriteLine($"the word length is: {word.Length}");
+        Console.WriteLine($"and the eord is: {word} ");
+        
+    }
+}
